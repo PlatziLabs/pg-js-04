066501d293d5a5c2fb95061a4464e5b8da9e8fd7
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-01 12:41:21 -0400

    :fire: remove solution

--- src/solution/index.js
+++
@@ -1,9 +0,0 @@
-import { runCode } from './exercise';
-
-(async () => {
-  const rta = await runCode();
-  document.getElementById("app").innerHTML = `
-    <h1>Refactor Challenge</h1>
-    <p>Result from exec runCode: ${rta}</p>
-  `;
-})();
